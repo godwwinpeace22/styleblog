68cd826eb9d67baaf8dc313da6d8e46214a219e0
godwin gabriel <godwinpeace22@gmail.com>
2018-08-16 09:27:13 +0100

    updated database params

--- config/db.php
+++ config/db.php
@@ -1,6 +1,12 @@
 <?php
   //create connection
-  $conn = mysqli_connect('localhost','root','','styleblog');
+  //$conn = mysqli_connect('localhost','root','','styleblog');
+  $server = getenv('database_host');
+  $username = getenv('database_username');
+  $password = getenv('database_password');
+  $db = getenv('database_name');
+
+  $conn = mysqli_connect($server,$username, $password, $db);
 
   // check connection
   if(mysqli_connect_errno()){
